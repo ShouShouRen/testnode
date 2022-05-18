dc98bea85a011ff535855cd966be8eb90bfc620f
Chen <peterchen2017125@gmail.com>
2022-05-18 16:15:28 +0800

    01

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
-// let a =1;
-// let b =2;
-// console.log(a+b);
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// setInterval(function(){
-//     console.log("hello");
-// })
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// const http = require("http");
-// http.createServer(function (request, response) {
-//     console.log(request.url)
-//     console.log('接收到網頁請球');
-//     response.writeHead(200, {"Content-Type":"text/plain"});
-//     response.write("hello world" + request.url);
-//     response.end();
-// }).listen(3000);
-// console.log("Server已開啟port: 3000.");
+var app = express();
 
-var http = require("http");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-http.createServer(function (request, response) {
-    console.log(request.url);
-    if(request.url == '/'){
-        console.log("接收到網頁請求");
-        response.writeHead(200,{"Content-Type":"text/HTML"});
-        response.write("<h1>index</h1>");
-        response.end();
-    }else if(request.url == '/search'){
-        console.log("接收到網頁請求");
-        response.writeHead(200,{"Content-Type":"text/HTML"});
-        response.write("<h1>search</h1>");
-        response.end();
-    }else{
-        response.writeHead(200,{"Content-Type":"text/HTML"});
-        response.write("404");
-        response.end();
-    }
-}).listen(process.env.PORT || 3000);
-console.log("Server已開啟port: 3000.");
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-
-
+module.exports = app;
