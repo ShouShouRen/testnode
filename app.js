7c22bdbdd5aaff70f483ea395bdcf176a27807de
Chen <peterchen2017125@gmail.com>
2022-05-18 14:50:38 +0800

    'update'

---
+++ app.js
@@ -0,0 +1,42 @@
+// let a =1;
+// let b =2;
+// console.log(a+b);
+
+// setInterval(function(){
+//     console.log("hello");
+// })
+
+// const http = require("http");
+// http.createServer(function (request, response) {
+//     console.log(request.url)
+//     console.log('接收到網頁請球');
+//     response.writeHead(200, {"Content-Type":"text/plain"});
+//     response.write("hello world" + request.url);
+//     response.end();
+// }).listen(3000);
+// console.log("Server已開啟port: 3000.");
+
+var http = require("http");
+
+http.createServer(function (request, response) {
+    console.log(request.url);
+    if(request.url == '/'){
+        console.log("接收到網頁請求");
+        response.writeHead(200,{"Content-Type":"text/HTML"});
+        response.write("<h1>index</h1>");
+        response.end();
+    }else if(request.url == '/search'){
+        console.log("接收到網頁請求");
+        response.writeHead(200,{"Content-Type":"text/HTML"});
+        response.write("<h1>search</h1>");
+        response.end();
+    }
+}).listen(process.env.PORT || 3000);
+console.log("Server已開啟port: 3000.");
+
+
+
+
+
+
+
